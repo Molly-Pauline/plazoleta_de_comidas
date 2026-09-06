bd9fca6c8eb9cbfa6c5f1b59e73b7436edba3096
ardilamelina8-blip <ardilamelina8@gmail.com>
2026-09-06 17:12:57 -0500

    HU2: implementar dominio y servicio de restaurantes

--- src/src/Main.java
+++ src/src/Main.java
@@ -1,13 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import model.Propietario;
+import model.Restaurante;
+import repository.PropietarioRepository;
+import repository.RestauranteRepository;
+import service.PropietarioService;
+import service.RestauranteService;
+import service.UsuarioValidationPort;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        PropietarioRepository propietarioRepository = new PropietarioRepository();
+        PropietarioService propietarioService = new PropietarioService(propietarioRepository);
+
+        Propietario propietario = new Propietario(
+                "Ana",
+                "García",
+                "12345678",
+                "+573001112233",
+                LocalDate.of(1990, 5, 20),
+                "ana@correo.com",
+                "ClaveSegura123"
+        );
+
+        try {
+            Propietario creado = propietarioService.crearPropietario(propietario, "ADMINISTRADOR");
+            System.out.println("Propietario creado: " + creado);
+
+            RestauranteRepository restauranteRepository = new RestauranteRepository();
+            UsuarioValidationPort usuarioValidationPort = idPropietario -> idPropietario.equals(1L) || idPropietario.equals(12345678L);
+            RestauranteService restauranteService = new RestauranteService(restauranteRepository, usuarioValidationPort);
+
+            Restaurante restaurante = new Restaurante(
+                    "La Casona",
+                    "123456789",
+                    "Calle 123 #45-67",
+                    "+573001234567",
+                    "https://example.com/logo.png",
+                    12345678L
+            );
+
+            Restaurante guardado = restauranteService.crearRestaurante(restaurante, "ADMINISTRADOR");
+            System.out.println("Restaurante creado: " + guardado.getNombre() + " - NIT: " + guardado.getNit());
+        } catch (Exception e) {
+            System.err.println("No se pudo completar la operación: " + e.getMessage());
+        }
     }
 }
---
+++ src/src/model/Restaurante.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Restaurante {
+    private String nombre;
+    private String nit;
+    private String direccion;
+    private String telefono;
+    private String urlLogo;
+    private Long idPropietario;
+
+    public Restaurante(String nombre, String nit, String direccion, String telefono, String urlLogo, Long idPropietario) {
+        this.nombre = nombre;
+        this.nit = nit;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.urlLogo = urlLogo;
+        this.idPropietario = idPropietario;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getNit() { return nit; }
+    public String getDireccion() { return direccion; }
+    public String getTelefono() { return telefono; }
+    public String getUrlLogo() { return urlLogo; }
+    public Long getIdPropietario() { return idPropietario; }
+
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setNit(String nit) { this.nit = nit; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+    public void setUrlLogo(String urlLogo) { this.urlLogo = urlLogo; }
+    public void setIdPropietario(Long idPropietario) { this.idPropietario = idPropietario; }
+}
